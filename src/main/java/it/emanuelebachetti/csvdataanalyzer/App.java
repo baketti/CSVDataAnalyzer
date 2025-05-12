5beaf351c51ead08559c0e0eed2070e605ae0fc2
baketti <e.bachetti1997@gmail.com>
2025-05-12 12:05:22 +0200

    Initial project setup

---
+++ src/main/java/it/emanuelebachetti/csvdataanalyzer/App.java
@@ -0,0 +1,13 @@
+package it.emanuelebachetti.csvdataanalyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
